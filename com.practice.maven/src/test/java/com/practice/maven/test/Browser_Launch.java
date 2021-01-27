d2909337e9c80cb85c788ac022ff5d27acedc61b
Ravikumar.Ganesan <Ravikumar.Ganesan@AGLS-LT-272.agilisium.com>
2021-01-27 11:21:42 +0530

    New Project Commit
    Signed-off-by: Ravikumar.Ganesan <Ravikumar.Ganesan@AGLS-LT-272.agilisium.com>

---
+++ com.practice.maven/src/test/java/com/practice/maven/test/Browser_Launch.java
@@ -0,0 +1,37 @@
+package com.practice.maven.test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Listeners;
+
+import atu.testng.reports.ATUReports;
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+import atu.testng.reports.logging.LogAs;
+import atu.testng.reports.utils.Utils;
+import atu.testng.selenium.reports.CaptureScreen;
+import atu.testng.selenium.reports.CaptureScreen.ScreenshotOf;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class, MethodListener.class })
+public class Browser_Launch {
+
+	public void b_launch()
+	{
+		System.out.println("Welcome to Maven World");
+		WebDriverManager.chromedriver().setup();
+		RemoteWebDriver driver= new ChromeDriver();
+		ATUReports.setWebDriver(driver);
+		ATUReports.indexPageDescription="My First ATU Report";
+		ATUReports.setAuthorInfo("Ravikumar", Utils.getCurrentTime(), "1.0");
+		driver.manage().window().maximize();
+		ATUReports.add("Browser window has been maximized successfully", LogAs.PASSED, new CaptureScreen(ScreenshotOf.BROWSER_PAGE));
+		driver.get("https://www.google.com/");
+		ATUReports.add("Specified URL has been opened successfully", LogAs.PASSED, new CaptureScreen(ScreenshotOf.BROWSER_PAGE));
+		System.out.println("Browser Title = "+driver.getTitle());
+		ATUReports.add("Browser Title = "+driver.getTitle(), LogAs.PASSED, new CaptureScreen(ScreenshotOf.DESKTOP));
+	}
+	
+	
+}
